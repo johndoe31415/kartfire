e8e0a02b6581b8f27455b13109450dab93c376ac
Johannes Bauer <joe@johannes-bauer.com>
2023-12-03 09:55:13 +0100

    Further progress on structure
    Advanced the structure a bit further.

--- kartfire/TestcaseCollection.py
+++ kartfire/TestcaseCollection.py
@@ -21,44 +21,13 @@
 #	Johannes Bauer <JohannesBauer@gmx.de>
 
 import json
+from .Testcase import Testcase
 
-class TestFixtureConfig():
-	def __init__(self, config: dict):
-		self._config = config
+class TestcaseCollection():
+	def __init__(self, testcases: list):
+		self._testcases = [ Testcase(testcase) for testcase in testcases ]
 
 	@classmethod
 	def load_from_file(cls, filename):
 		with open(filename) as f:
 			return cls(json.load(f))
-
-	@property
-	def setup_name(self):
-		return self._config.get("setup_name", "setup")
-
-	@property
-	def solution_name(self):
-		return self._config.get("solution_name", "solution")
-
-	@property
-	def max_memory_mib(self):
-		return self._config.get("max_memory_mib", 1024)
-
-	@property
-	def host_memory_usage_percent(self):
-		return self._config.get("host_memory_usage_percent", 100)
-
-	@property
-	def max_concurrent_processes(self):
-		return self._config.get("max_concurrent_processes", 8)
-
-	@property
-	def max_build_time_secs(self):
-		return self._config.get("max_build_time_secs", 30)
-
-	@property
-	def minimum_testcase_time(self):
-		return self._config.get("minimum_testcase_time", 0.5)
-
-	@property
-	def reference_time_factor(self):
-		return self._config.get("reference_time_factor", 10)
--- kartfire/__main__.py
+++ kartfire/__main__.py
@@ -22,15 +22,22 @@
 
 import sys
 from .FriendlyArgumentParser import FriendlyArgumentParser
+from .TestFixtureConfig import TestFixtureConfig
+from .TestcaseRunner import TestcaseRunner
+from .TestcaseCollection import TestcaseCollection
 
 def main():
 	parser = FriendlyArgumentParser(description = "Kartfire test running application.")
 	parser.add_argument("-c", "--test-fixture-config", metavar = "filename", help = "Specify a specific test fixture configuration to use.")
-	parser.add_argument("-t", "--testcase", metavar = "filename", action = "append", required = True, help = "Testcase definition JSON file. Can be given multiple times to join testcases. Mandatory argument.")
+	parser.add_argument("-f", "--testcase-file", metavar = "filename", action = "append", required = True, help = "Testcase definition JSON file. Can be given multiple times to join testcases. Mandatory argument.")
 	parser.add_argument("-v", "--verbose", action = "count", default = 0, help = "Increases verbosity. Can be specified multiple times to increase.")
 	parser.add_argument("testcase", nargs = "+", help = "Directory/directories that should be run as a testcase inside containers.")
 	args = parser.parse_args(sys.argv[1:])
 
+	testcase_collections = [ TestcaseCollection.load_from_file(tc_filename) for tc_filename in args.testcase_file ]
+	test_fixture_config = TestFixtureConfig.load_from_file(args.test_fixture_config)
+	tcr = TestcaseRunner(testcase_collections = testcase_collections, test_fixture_config = test_fixture_config)
+	tcr.run()
 
 if __name__ == "__main__":
 	main()
