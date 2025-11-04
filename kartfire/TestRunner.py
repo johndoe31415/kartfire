67869070be31a8d862f243b3383e403de8cc9ddc
Johannes Bauer <joe@johannes-bauer.com>
2025-11-04 10:36:10 +0100

    Correctly record build time
    Previously, we erroneously recorded the tarring time as the build time.
    Fixed.

--- kartfire/TestRunner.py
+++ kartfire/TestRunner.py
@@ -145,7 +145,7 @@ class TestRunner():
 						else:
 							exec_result.testrun_status = TestrunStatus.Failed
 						exec_result.error_details = exception
-					elif msg_type == "time":
+					elif (msg_type == "time") and (json_data["msg"]["target"] in [ "build", "run" ]):
 						exec_result.runtime_secs = json_data["msg"]["time"]
 				else:
 					# Regular parsable JSON message
